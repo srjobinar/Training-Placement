acafe499b3fea2098f168bfb13ba231dd5bf6cbe
srjobinar <jobinar007@gmail.com>
2016-05-19 23:08:43 +0530

    Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fulltitle(title=" ")
+    if title.empty?
+      "Training And Placement Cell"
+    else
+      title+" | Training And Placement Cell"
+    end
+  end
 end
